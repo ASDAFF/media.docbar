9f4939e18ee664aaab125d6bfca8af8940dc534c
gr1ff0n <gr1ff0n@users.noreply.github.com>
2014-08-03 17:11:51 +0400

    Update template.php
    Fixed 'ru' language encoding on GitHub

--- alkom/media.docbar/templates/.default/lang/ru/template.php
+++ alkom/media.docbar/templates/.default/lang/ru/template.php
@@ -1,6 +1,6 @@
 <?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die(); ?>
 <?
-$MESS ['ALKOM_DOCBAR_VIEW_ITEM'] = "Ïîñìîòðåòü";
-$MESS ['ALKOM_DOCBAR_DOWNLOAD_ITEM'] = "Ñêà÷àòü";
-$MESS ['ALKOM_DOCBAR_ACCESS_DENIED'] = "Äîñòóï çàïðåùåí!";
+$MESS ['ALKOM_DOCBAR_VIEW_ITEM'] = "ÐŸÐ¾ÑÐ¼Ð¾Ñ‚Ñ€ÐµÑ‚ÑŒ";
+$MESS ['ALKOM_DOCBAR_DOWNLOAD_ITEM'] = "Ð¡ÐºÐ°Ñ‡Ð°Ñ‚ÑŒ";
+$MESS ['ALKOM_DOCBAR_ACCESS_DENIED'] = "Ð”Ð¾ÑÑ‚ÑƒÐ¿ Ð·Ð°Ð¿Ñ€ÐµÑ‰ÐµÐ½!";
 ?>

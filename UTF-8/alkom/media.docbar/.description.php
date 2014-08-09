e6c9e69f0c9a42f262117889ffdd366717c48336
gr1ff0n <toxic.dm@gmail.com>
2014-08-09 17:07:33 +0400

    Added folders for UTF-8

---
+++ UTF-8/alkom/media.docbar/.description.php
@@ -0,0 +1,14 @@
+<?
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+$arComponentDescription = array(
+	"NAME" => GetMessage("ALKOM_DOCBAR_NAME"),
+	"DESCRIPTION" => GetMessage("ALKOM_DOCBAR_DESCRIPTION"),
+	"ICON" => "/images/icon.gif",
+	"VERSION" => "1.02",
+	"PATH" => array(
+		"ID" => "AlKom",
+	),
+);
+
+?>
